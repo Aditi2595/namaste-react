d847b1c34b3dfcc0321e620a5d53a228b691510a
Aditi Tewari <adi.tiwari2511@gmail.com>
2023-04-23 17:41:26 +0530

    Episode2&Partial3

--- App.js
+++ App.js
@@ -10,18 +10,31 @@
  *      </div> 
  * </div>
  */
+import React from "react"
+import ReactDOM from "react-dom/client"
 
 
+// const parent = React.createElement("div", {id:"parent"},[
+// React.createElement("div", {id:"child"},[
+// React.createElement("h1", {}, "I'm h1 tag"), 
+// React.createElement("h2", {}, "I'm h2 tag")]
+// ),
+// React.createElement("div", {id:"child2"},[
+//     React.createElement("h1", {}, "I'm h1 tag"), 
+//     React.createElement("h2", {}, "I'm h2 tag")]
+//     )]);
+// console.log(parent);
 
-const parent = React.createElement("div", {id:"parent"},[
-React.createElement("div", {id:"child"},[
-React.createElement("h1", {}, "I'm h1 tag"), 
-React.createElement("h2", {}, "I'm h2 tag")]
-),
-React.createElement("div", {id:"child2"},[
-    React.createElement("h1", {}, "I'm h1 tag"), 
-    React.createElement("h2", {}, "I'm h2 tag")]
-    )]);
-console.log(parent);
+const heading = React.createElement("h1", {id:"heading"}, "Namaste React");
+console.log(heading);
+
+// JSX (transpiled before it reaches the JS) - Parcel - Babel
+// JAX => React.createElement => ReactElement - JS Object => HTML(render)
+// to write JSX in multiple lines, we need to wrap it inside ()
+
+
+const jsxHeading = <h1 id="heading" className="head" tabIndex="1">Namaste React through JSX</h1>;
+console.log(jsxHeading)
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(jsxHeading);
--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # episode 1
+
+# episode 2
+# Parcel
+- Dev Build
+- Local server
+- HMR : Hot Module Replacement
+- File watching algortihms - written in C++
+- Caching : Faster Builds
+- Image Optimization
+- Minification
+- Bundling
+- Compressing
+- Content hashing
+- Code splitting
+- Differential Bundling - support older browsers
+- Diagnostics
+- Error Handling
+- HTTPs
+Tree shaking - remove unwanted code
